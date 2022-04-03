ee514badc14eb61ce1294e2a0b6add0a0d444e16
Ewan Redfield <29620734+chryoung@users.noreply.github.com>
2022-04-04 03:24:43 +0800

    Update favicon and dev config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # config.action_view.annotate_rendered_view_with_filenames = true
 
   # Uncomment if you wish to allow Action Cable access from any origin.
-  config.action_cable.disable_request_forgery_protection = true
+  # config.action_cable.disable_request_forgery_protection = true
   
-  config.web_console.whitelisted_ips = %w( 0.0.0.0/0 ::/0 )
+  # config.web_console.allowed_ips = %w( 0.0.0.0/0 ::/0 )
 end
